11f57b11e76348ae42334c177c8696a3ed5822c7
hammershock <hammershock@163.com>
2024-05-26 22:17:17 +0800

    modify the output path

--- demo1.py
+++ demo1.py
@@ -10,7 +10,7 @@ from bili_spider import make_chrome_browser, get_user_videos
 if __name__ == '__main__':
     mid = 1629347259  # 用户id
 
-    with make_chrome_browser(executable_path="./chromedriver") as browser, open("info.txt", "w") as f:
+    with make_chrome_browser(executable_path="./chromedriver", headless=False) as browser, open("info.txt", "w") as f:
         for attrs in get_user_videos(browser, mid):
-            f.write(" ".join(attrs) + '\n')
-        os.rename("info.txt", f"{attrs[1]}.txt")
+            f.write("\t".join(attrs) + '\n')
+        os.rename("info.txt", f"{attrs[2]}.txt")
